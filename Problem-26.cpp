e37e142146c80994c9729fb4f502018ab6c19f32
Richa <agrawalricha013@gmail.com>
2023-03-29 20:15:28 +0530

    Remove Duplicates from sorted array

---
+++ Problem-26.cpp
@@ -0,0 +1,28 @@
+// Remove Duplicates from sorted array
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        int j = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[j] != nums[i]){
+                nums[++j] = nums[i];
+            }
+        }
+        return j+1;
+    }
+};
+
+int main(){
+    Solution soln;
+    vector<int> nums = {1, 1, 3, 3, 7, 7, 8, 8, 9, 9, 10, 10};
+
+    cout<<"After removing duplicates: "<<soln.removeDuplicates(nums);
+
+    return 0;
+}
