4a7ffa5550ad141072ecb668d2303aba7c8cfecb
Richa <agrawalricha013@gmail.com>
2023-03-29 20:20:21 +0530

    Maximum Subarray

---
+++ Problem-53.cpp
@@ -0,0 +1,36 @@
+// Maximum Subarray
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        int maxSum = INT_MIN;
+        int currSum = 0;
+        
+        for(int i = 0; i < n; i++)
+        {
+            currSum += nums[i];
+            
+            maxSum = max(maxSum, currSum);
+            
+            if(currSum < 0)
+            {
+                currSum = 0;
+            }
+        }
+        return maxSum;
+    }
+};
+
+int main(){
+    vector<int> nums = {-2,1,-3,4,-1,2,1,-5,4}; 
+
+    Solution soln;
+
+    cout<<"Maximum Subarray: "<<soln.maxSubArray(nums)<<endl;
+    return 0;
+}
